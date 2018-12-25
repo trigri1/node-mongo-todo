377a23411ed179e56b223620a5d04a3bcec2b6d6
gohar-ali <gohar.ali@starzplayarabia.com>
2018-12-25 13:10:10 +0500

    add user route and token creation logic

---
+++ playground/hashing.js
@@ -0,0 +1,38 @@
+const {
+    SHA256
+} = require('crypto-js');
+
+const jwt = require('jsonwebtoken');
+
+var data = {
+    id: 1
+};
+var encoded = jwt.sign(data, '333');
+console.log('encoded', encoded);
+decoded = jwt.verify(encoded, '333')
+console.log('decoded', decoded);
+// var message = 'I am user';
+
+// var hash = SHA256(message);
+
+// console.log(`${message}`);
+// console.log(`${hash}`);
+
+// var data = {
+//     id: 6
+// };
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'secter').toString()
+// };
+
+// token.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'secter').toString();
+
+// if (token.hash === resultHash) {
+//     console.log('data was not changed');
+// } else {
+//     console.log('data was changed');
+// }
--- server/server.js
+++ server/server.js
@@ -147,6 +147,19 @@ app.patch('/todos/:id', (req, res) => {
 
 })
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+       return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => {
+        res.status(400).send(err)
+    });
+});
+
 if (!module.parent) {
     app.listen(port, () => {
         console.log(`listening at port ${port}`);
