0e95e8173c7d3646ff89490d4ef626f0ad975029
gohar-ali <gohar.ali@starzplayarabia.com>
2019-01-01 20:32:52 +0500

    environment variables set for local and heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,18 @@
 var env = process.env.NODE_ENV || 'dev';
 
-if (env === 'dev') {
-    process.env.PORT = 3000;
-    process.env.MONGOLAB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGOLAB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'dev' || env === 'test') {
+    var config = require('./config.json');
+    var configEnv = config[env];
+
+    Object.keys(configEnv).forEach((key) => {
+        process.env[key] = configEnv[key];
+    });
 }
+
+// if (env === 'dev') {
+//     process.env.PORT = 3000;
+//     process.env.MONGOLAB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//     process.env.PORT = 3000;
+//     process.env.MONGOLAB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
